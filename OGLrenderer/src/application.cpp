2688ace656c44f786d931f1c3bbd8b43dff40e6b
Lucas <lucashahne06@gmail.com>
2026-07-12 11:51:22 +0200

    Abtracted vertex arrays into its own seperate class

--- OGLrenderer/src/application.cpp
+++ OGLrenderer/src/application.cpp
@@ -9,6 +9,7 @@
 #include "Renderer.h";
 #include "VertexBuffer.h";
 #include "IndexBuffer.h";
+#include "VertexArray.h";
 
 struct ShaderProgramSource {
     std::string VertexSource;
@@ -87,7 +88,7 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
 
 static void FramebufferSizeCallback(GLFWwindow *window, int width, int height)
 {
-    glViewport(0, 0, width, height);
+    GLCall(glViewport(0, 0, width, height));
 }
 
 int main(void)
@@ -137,14 +138,12 @@ int main(void)
             2, 3, 0
         };
 
-        unsigned int vao;
-        GLCall(glGenVertexArrays(1, &vao));
-        GLCall(glBindVertexArray(vao));
-
+        VertexArray va;
         VertexBuffer vb(positions, 2 * 4 * sizeof(float));
 
-        GLCall(glEnableVertexAttribArray(0));
-        GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
 
         IndexBuffer ib(indices, 3 * 2);
 
@@ -169,7 +168,7 @@ int main(void)
             GLCall(glUseProgram(shader));
             GLCall(glUniform4f(location, r, 0.4f, 0.8f, 1.0f));
 
-            GLCall(glBindVertexArray(vao));
+            va.Bind();
             ib.Bind();
 
             GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
